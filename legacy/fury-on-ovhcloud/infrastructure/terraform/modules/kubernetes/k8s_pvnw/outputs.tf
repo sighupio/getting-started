ba9e2fdec25479d36b0ac0de9e01890d016d8210
Samuele Chiocca <samuele@sighup.io>
2025-03-24 20:47:08 +0100

    chore: remove legacy getting started guides (too old to be maintained)

--- legacy/fury-on-ovhcloud/infrastructure/terraform/modules/kubernetes/k8s_pvnw/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "kubeconfig_file" {
-  description = "kubeconfig file"
-  value       = ovh_cloud_project_kube.kube.kubeconfig
-}
--- legacy/fury-on-ovhcloud/infrastructure/terraform/providers.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  required_providers {
-    local = {
-      version = "~> 2.2.3"
-    }
-  }
-  required_version = "~> 1.3.6"
-}
